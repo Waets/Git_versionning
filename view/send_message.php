726fa1f90eca0c76ff5a3f8132277fd3381cee3c
Joshua Tabet <Joshua@MacBook-Pro-de-Joshua.local>
2016-09-14 16:45:48 +0200

    change size of contact form

--- view/send_message.php
+++ view/send_message.php
@@ -13,17 +13,17 @@
 <form method="POST">
 					<div class="form-input">
 						<span class="username">
-							<p align="center"><input type="text" name="nom" placeholder="nom"></p>
+							<p align="center"><input type="text" name="nom" placeholder="nom" size="50"></p>
 						</span>
 					</div>
 					<div class="form-input">
 						<span class="email">
-							<p align="center"><input type="text" name="email" placeholder="Email"></p>
+							<p align="center"><input type="text" name="email" placeholder="Email" size="50"></p>
 						</span>
 					</div>
 					<div class="form-input">
 						<span class="password">
-							<p align="center"><input type="text" name="message" placeholder="message" cols="50" rows="10"></textarea></td></p>
+							<p align="center"><input type="text" name="message" placeholder="message" cols="50" rows="10" size="50"></textarea></td></p>
 						</span>
 						<p align="center"><input type="reset" name="Submit" value="Réinitialiser le formulaire"></p>
 </center></td>
