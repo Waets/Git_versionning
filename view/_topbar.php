c8e21913dea35759ebd7d5f70bfbccaadc522386
Unknown <greenwaets@gmail.com>
2016-09-14 14:49:19 +0200

    Added Facebook and Twitter logo

--- view/_topbar.php
+++ view/_topbar.php
@@ -7,8 +7,8 @@
 						<a href="index.php" class="logo"><i class="fa fa-soundcloud"></i></a>
 					</div>
 					<div class="col-xs-10 col-sm-10 col-md-11 col-lg-11">
-						<a href="https://twitter.com/soundcloud?lang=fr" class="btn">Twitter</a>
-						<a href="https://www.facebook.com/SoundCloud/" class="btn">Facebook</a>
+						<a href="https://twitter.com/soundcloud?lang=fr" class="btn"><img src="images/twitterlogo.png" width="25" height="25" alt="logo twitter" /></a>
+						<a href="https://www.facebook.com/SoundCloud/" class="btn"><img src="images/fblogo.png" width="25" height="25" alt="logo facebook" /></a>
 						<a href="logout.php" class="btn">Déconnexion</a>
 						<a href="edit_image.php" class="btn">Profil</a>
 						<a href="add_music.php" class="btn"><i class="fa fa-plus"></i> Ajouter une musique</a>
