55a01734e63dd5096ca0752c99d0a961eebca839
Unknown <greenwaets@gmail.com>
2016-09-15 10:46:10 +0200

    Translation french >english

--- view/send_message.php
+++ view/send_message.php
@@ -3,13 +3,13 @@
 <?php include '_topbar.php'; ?>
 
 
-<p align="center"><strong>Contact</strong></p>
+<br><p align="center"><strong>Contact us</strong></p><br>
 
 
 <form method="POST">
 					<div class="form-input">
 						<span class="username">
-							<p align="center"><input type="text" name="nom" placeholder="nom" size="50"></p>
+							<p align="center"><input type="text" name="nom" placeholder="Name" size="50"></p>
 						</span>
 					</div>
 					<div class="form-input">
@@ -19,12 +19,12 @@
 					</div>
 					<div class="form-input">
 						<span class="password">
-							<p align="center"><input type="text" name="message" placeholder="message" cols="50" rows="10" size="50"></textarea></td></p>
+							<p align="center"><input type="text" name="message" placeholder="Message" cols="50" rows="10" size="50"></textarea></td></p>
 						</span>
-						<p align="center"><input type="reset" name="Submit" value="Réinitialiser le formulaire"></p>
+						<p align="center"><input type="reset" name="Submit" value="Reinitiate the form"></p>
 </center></td>
 <td width="41%"><center>
-<input type="submit" name="Submit" value="Envoyer">
+<input type="submit" name="Submit" value="Submit">
 </center></td>
 					</div> 
 
