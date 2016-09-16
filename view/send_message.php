0ee57500f805b65f84ca86db9c7d12c73f48bbd8
Unknown <joshua@tabetozor.com>
2016-09-16 14:50:23 +0200

    change contact us text

--- view/send_message.php
+++ view/send_message.php
@@ -9,10 +9,10 @@
 <form action="mailto:greenwaets@gmail.com" method="post" name="contact">
 					
 <form>
-		<input name="name" placeholder="What is your name?" class="name" required />
+		<input name="name" placeholder="What's your name?" class="name" required />
 		<input name="emailaddress" placeholder="What is your email?" class="email" type="email" required />
     <textarea rows="4" cols="60" name="subject" placeholder="Please enter your message" class="message" required></textarea>
-    <input name="submit" class="btn" type="submit" value="Send" width="20" />
+    <input name="submit" class="btn" type="submit" value="Send" />
 </form>
 
 
