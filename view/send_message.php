ea1c16e2a7b6435b11784f4e427304dd9d40e173
Unknown <joshua@tabetozor.com>
2016-09-16 14:49:23 +0200

    fixed topbar

--- view/send_message.php
+++ view/send_message.php
@@ -12,7 +12,7 @@
 		<input name="name" placeholder="What is your name?" class="name" required />
 		<input name="emailaddress" placeholder="What is your email?" class="email" type="email" required />
     <textarea rows="4" cols="60" name="subject" placeholder="Please enter your message" class="message" required></textarea>
-    <input name="submit" class="btn" type="submit" value="Send" />
+    <input name="submit" class="btn" type="submit" value="Send" width="20" />
 </form>
 
 
