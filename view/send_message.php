daf8fd224e5f892cae0a3feb6d02655020c70649
Unknown <joshua@tabetozor.com>
2016-09-16 14:47:58 +0200

    fixed contact

--- view/send_message.php
+++ view/send_message.php
@@ -11,7 +11,7 @@
 <form>
 		<input name="name" placeholder="What is your name?" class="name" required />
 		<input name="emailaddress" placeholder="What is your email?" class="email" type="email" required />
-    <textarea rows="4" cols="50" name="subject" placeholder="Please enter your message" class="message" required></textarea>
+    <textarea rows="4" cols="60" name="subject" placeholder="Please enter your message" class="message" required></textarea>
     <input name="submit" class="btn" type="submit" value="Send" />
 </form>
 
